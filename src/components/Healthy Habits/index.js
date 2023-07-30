539529f87241274463458c2fc26904bba0dc3ea4
kyle-mucerino <kyle.mucerino@gmail.com>
2023-07-30 10:50:46 -0700

    Healthy Habits info

--- src/components/Healthy Habits/index.js
+++ src/components/Healthy Habits/index.js
@@ -5,7 +5,7 @@ const HealthyHabits = () => {
     <div>
       <h2>Healthy Habits</h2>
       <div className="container">
-        <p>You don't need to follow some strictly crazy diet to achieve your goals...but you do have to make some sacrifices.</p>
+        <p>You don't need to follow some strictly crazy diet to achieve your goals...but you do have to make some sacrifices. I am a big believer in fasting and its beneftis for building lean muscle and gut health. You don't have to wait until noon to eat, but what you do have to do is eat within a 9-12 hour window. 9 hour window is best but that can be unrealistic, so you just want to make sure you stay within a 12 hour window. What does that mean? Well your window starts with the first thing other than water you put in your body, so if you wake up and have coffee at 7 am you should aim for eating nothing past 7pm. In my experience, if you can ween yourself off of early morning caloric intake and hold off until even 8 or 9 you put yourself in much better position to be able to stay within your window. If you do this 6 days out of the week, I promise you'll see the results and more importantly, feel them.</p>
       </div>
     </div>
   );
